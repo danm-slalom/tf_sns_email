242a23de890322470b61a3b78cb1a7c39becd9ba
Dan Mazur <dan.mazur@slalom.com>
2017-06-05 17:04:53 -0500

    Added required tags

--- main.tf
+++ main.tf
@@ -14,7 +14,63 @@ resource "aws_cloudformation_stack" "sns-topic" {
     name = "${var.stack_name}"
     template_body = "${template_file.cloudformation_sns_stack.rendered}"
 
-    tags {
-        Owner = "${var.owner}"
-    }
+  tag {
+    key                 = "DAC"
+    value               = "${var.tags_dac}"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Data Classification"
+    value               = "${var.tags_data_classification}"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Description"
+    value               = "${var.tags_description}"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Environment"
+    value               = "${var.tags_environment}"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "GBL"
+    value               = "${var.tags_gbl}"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Deployment Name"
+    value               = "${var.tags_name}"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Owner"
+    value               = "${var.tags_owner}"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Pyxis"
+    value               = "${var.tags_pyxis}"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Version"
+    value               = "${var.tags_version}"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Static"
+    value               = "${var.tags_static}"
+    propagate_at_launch = true
+  }
 }
--- variables.tf
+++ variables.tf
@@ -18,3 +18,43 @@ variable "protocol" {
 variable "stack_name" {
     type = "string"
 }
+
+variable "tags_dac" {
+    type = "string"
+}
+
+variable "tags_data_classification" {
+    type = "string"
+}
+
+variable "tags_description" {
+    type = "string"
+}
+
+variable "tags_environment" {
+    type = "string"
+}
+
+variable "tags_gbl" {
+    type = "string"
+}
+
+variable "tags_name" {
+    type = "string"
+}
+
+variable "tags_owner" {
+    type = "string"
+}
+
+variable "tags_pyxis" {
+    type = "string"
+}
+
+variable "tags_static" {
+    type = "string"
+}
+
+variable "tags_version" {
+    type = "string"
+}
